a3aa194731c30c3bd2a6a712283b58bdc72b77bf
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2020-09-06 16:24:31 -0300

    - Incluir classe de teste.

---
+++ GameSaveManager.Core.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+
+using Xunit;
+
+namespace GameSaveManager.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
