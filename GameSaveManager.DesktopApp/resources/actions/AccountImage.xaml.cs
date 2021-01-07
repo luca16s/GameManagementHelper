81dd735938f5ebb552c1b6fa1b1b94249e59f0cd
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-01-06 22:05:18 -0300

    [52]
    - Remover páginas desnecessárias

--- GameSaveManager.DesktopApp/resources/actions/AccountImage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameSaveManager.DesktopApp.resources.actions
-{
-    using System.Windows.Controls;
-
-    public partial class AccountImage : UserControl
-    {
-        public AccountImage() => InitializeComponent();
-    }
-}
