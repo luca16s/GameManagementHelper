a405982be48ddc133772dc63518f94c98c1372f7
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-01-06 21:59:53 -0300

    [52]
    - Incluir componente de botão para a tela inicial do programa.

---
+++ GameSaveManager.DesktopApp/Components/ButtonBaseComponent.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace GameSaveManager.DesktopApp.Components
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+
+    public partial class ButtonBaseComponent : UserControl
+    {
+        public ButtonBaseComponent() => InitializeComponent();
+
+        public Action OnClickEvent;
+
+        public string Data
+        {
+            get => (string)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+
+        public static DependencyProperty DataProperty =
+           DependencyProperty.Register(nameof(Data), typeof(string), typeof(ButtonBaseComponent));
+
+        public event RoutedEventHandler Click
+        {
+            add { Button.AddHandler(ButtonBase.ClickEvent, value); }
+            remove { Button.RemoveHandler(ButtonBase.ClickEvent, value); }
+        }
+    }
+}
