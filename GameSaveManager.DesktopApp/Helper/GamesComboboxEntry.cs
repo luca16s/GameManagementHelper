ad8405f5ec0f41b774aa71100ee77b31825fc29d
Gian Luca Da Silva Figueiredo <luca.gian16@outlook.com>
2021-01-10 14:09:51 -0300

    [58]
    - Incluir opção de se adicionar jogos apenas por editar o arquivo
      gameslist.json

---
+++ GameSaveManager.DesktopApp/Helper/GamesComboboxEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace GameSaveManager.DesktopApp.Helper
+{
+    using GameSaveManager.Core.Models;
+
+    public class GamesComboboxEntry
+    {
+        public string Title { get; set; }
+        public GameInformationModel Game { get; set; }
+    }
+}
